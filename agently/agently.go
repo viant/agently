b93f4ab30a347a21976bc8f31fbe9c6344343d71
adranwit <adrianwit@gmail.com>
2025-06-03 10:49:20 -0700

    initial commit

--- agently/agently.go
+++ agently/agently.go
@@ -7,13 +7,8 @@ import (
 	_ "github.com/viant/afsc/s3"
 	"github.com/viant/agently/cmd/agently"
 	"os"
-	"path"
 )
 
 func main() {
-	baseDir := "/Users/awitas/go/src/github.com/viant/agently"
-
-	os.Args = []string{"", "chat", "-a" + path.Join(baseDir, "llm.log"), "-w" + path.Join(baseDir, "workflow.log")}
 	agently.RunWithCommands(os.Args[1:])
-
 }
