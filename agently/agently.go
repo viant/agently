206f27307fe706ff443ea3864685e57df8e6d53c
adranwit <adrianwit@gmail.com>
2025-07-21 07:34:05 +0200

    enhanced logic

--- agently/agently.go
+++ agently/agently.go
@@ -5,14 +5,16 @@ import (
 	_ "github.com/viant/afsc/gcp"
 	_ "github.com/viant/afsc/gs"
 	_ "github.com/viant/afsc/s3"
+	"github.com/viant/agently/cmd/agently"
+	"os"
 )
 
 func main() {
 
 	//os.Setenv("AGENTLY_ROOT", "/Users/awitas/go/src/github.com/viant/agently/ag")
 
-	//os.Args = []string{"", "serve", "-p=ask"}
+	//os.Args = []string{"", "chat"}
 	//os.Args = []string{"", "chat", "--reset-logs", "-q='hi,what country  is my favourite city ?"}
 	//os.Args = []string{"", "serve"}
-	//agently.RunWithCommands(os.Args[1:])
+	agently.RunWithCommands(os.Args[1:])
 }
