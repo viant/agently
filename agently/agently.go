483454f4d6a53c8d582710920d4c61f66f29f08e
adranwit <adrianwit@gmail.com>
2025-08-16 08:15:24 -0700

    updated error handling

---
+++ agently/agently.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"os"
+
+	_ "github.com/viant/afsc/aws"
+	_ "github.com/viant/afsc/gcp"
+	_ "github.com/viant/afsc/gs"
+	_ "github.com/viant/afsc/s3"
+	"github.com/viant/agently/cmd/agently"
+)
+
+func main() {
+	agently.RunWithCommands(os.Args[1:])
+}
