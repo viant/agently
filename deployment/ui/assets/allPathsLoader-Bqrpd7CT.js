c49b451ab9a64ccfd4621421e796e0aebe248199
Michał Filipowicz <mfilipowicz@interia.eu>
2026-01-30 18:24:16 +0100

    updated ui

---
+++ deployment/ui/assets/allPathsLoader-Bqrpd7CT.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/allPaths-HCG0WUue.js","assets/index-FAkybV_U.js","assets/index-aE8xS9Qc.js","assets/main-DHeMffjS.js"])))=>i.map(i=>d[i]);
+import{_ as e}from"./main-DHeMffjS.js";const s=async(t,a)=>{const{getIconPaths:o}=await e(async()=>{const{getIconPaths:r}=await import("./allPaths-HCG0WUue.js");return{getIconPaths:r}},__vite__mapDeps([0,1,2,3]));return o(t,a)};export{s as allPathsLoader};
