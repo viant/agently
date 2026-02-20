6756ed21cc8ca0f24c9bbbb87615fe16bd40d2e8
Michał Filipowicz <mfilipowicz@interia.eu>
2026-02-20 16:57:56 +0100

    added schedule timeout - ui

--- deployment/ui/assets/allPaths-2rlW7oib.js
+++
@@ -1,1 +0,0 @@
-import{I as s}from"./index-voJy5fZe.js";import{I as r}from"./index-BZITDwoa.js";import{a as n,b as c}from"./main-CawKbdVt.js";function i(a,t){const o=n(a);return t===c.STANDARD?s[o]:r[o]}export{s as IconSvgPaths16,r as IconSvgPaths20,i as getIconPaths};
--- deployment/ui/assets/allPathsLoader-BGLtf1Kq.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/allPaths-ZON3bXwj.js","assets/index-BmDjQHI0.js","assets/index-CzNKtOwn.js","assets/main-CawKbdVt.js"])))=>i.map(i=>d[i]);
-import{_ as e}from"./main-CawKbdVt.js";const s=async(t,a)=>{const{getIconPaths:o}=await e(async()=>{const{getIconPaths:r}=await import("./allPaths-ZON3bXwj.js");return{getIconPaths:r}},__vite__mapDeps([0,1,2,3]));return o(t,a)};export{s as allPathsLoader};
