f9a17e166823e06c3c05e470669fbd35d3fe3474
Michał Filipowicz <mfilipowicz@interia.eu>
2026-03-31 02:25:42 +0200

    generated ui

---
+++ deployment/ui/assets/allPaths-BaJLg7XT.js
@@ -0,0 +1,1 @@
+import{I as a}from"./index-BmDjQHI0.js";import{I as s}from"./index-CzNKtOwn.js";import{p as m,I as p}from"./index-nnG4CNFQ.js";import"./react-vendor-Cgm7w9Fp.js";import"./chart-vendor-B52YOStV.js";import"./diagram-vendor-DAKLQNqy.js";function S(t,r){const o=m(t);return r===p.STANDARD?a[o]:s[o]}export{a as IconSvgPaths16,s as IconSvgPaths20,S as getIconPaths};
