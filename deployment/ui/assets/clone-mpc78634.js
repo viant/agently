278819f5550365cf74cdf652418dfee351a199df
adranwit <adrianwit@gmail.com>
2026-06-11 06:03:24 -0700

    - updated mobile, added autonomous controller

--- deployment/ui/assets/clone-mpc78634.js
+++
@@ -1,1 +0,0 @@
-import{b as r}from"./_baseUniq-CMDE0jVz.js";var e=4;function a(o){return r(o,e)}export{a as c};
---
+++ deployment/ui/assets/clone-oHAuKV9_.js
@@ -0,0 +1,1 @@
+import{b as r}from"./_baseUniq-iw6G6KMH.js";var e=4;function a(o){return r(o,e)}export{a as c};
