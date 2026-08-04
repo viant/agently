b3da8886b0b4be60ff02877c8d09f80f4e21b815
Michał Filipowicz <pol-mfilipowicz@viant-contractors.com>
2026-08-04 19:37:46 +0200

    build(ui): refresh deployment assets after reporting merge

---
+++ deployment/ui/assets/splitPathsBySizeLoader-DvBJrCfh.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./forge-reporting-NQ2J4nP4.js";import{p as _,I as s}from"./index-DjFAer8P.js";import"./code-editor-rcMmja5o.js";const c=async(a,r)=>{const i=_(a);let t;return r===s.STANDARD?t=await o(()=>import("./index-Bck4KEe_.js").then(e=>e.I),[]):t=await o(()=>import("./index-W58HgMhY.js").then(e=>e.I),[]),t[i]};export{c as splitPathsBySizeLoader};
