748997aecec97403895efe1bfe354e6eb4b50880
Michał Filipowicz <pol-mfilipowicz@viant-contractors.com>
2026-08-05 16:34:00 +0200

    feat(cli): add scratchpad root URI flag to scheduler

--- cmd/agently/scheduler_test.go
+++ cmd/agently/scheduler_test.go
@@ -2,9 +2,37 @@ package agently
 
 import (
 	"testing"
+	"time"
 
 	"github.com/jessevdk/go-flags"
 )
+
+func TestSchedulerRunCmd_ScratchpadRootURI(t *testing.T) {
+	for _, flag := range []string{"-s", "--scratchpad-root-uri"} {
+		t.Run(flag, func(t *testing.T) {
+			cmd := &SchedulerRunCmd{}
+			parser := flags.NewParser(cmd, flags.HelpFlag|flags.PassDoubleDash)
+
+			_, err := parser.ParseArgs([]string{
+				flag, "gs://scratchpad-bucket/users/${userID}",
+			})
+			if err != nil {
+				t.Fatalf("parse args: %v", err)
+			}
+
+			options, err := cmd.schedulerRunOptions()
+			if err != nil {
+				t.Fatalf("build scheduler options: %v", err)
+			}
+			if got, want := options.ScratchpadRootURI, "gs://scratchpad-bucket/users/${userID}"; got != want {
+				t.Fatalf("ScratchpadRootURI = %q, want %q", got, want)
+			}
+			if got, want := options.Interval, 30*time.Second; got != want {
+				t.Fatalf("Interval = %s, want %s", got, want)
+			}
+		})
+	}
+}
 
 func TestOptionsInitScheduler(t *testing.T) {
 	opts := &Options{}
