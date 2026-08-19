a7239dd18df00e3558fc710a2bba1c9bb4841cea
adranwit <adrianwit@gmail.com>
2026-08-19 13:13:57 -0700

    - updated mobile sdk

---
+++ deployment/ui/assets/splitPathsBySizeLoader-D27dvUqE.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./forge-reporting-DJ0eM5cJ.js";import{p as _,I as s}from"./index-CHTqGB7D.js";import"./code-editor-D5k8MIvU.js";const c=async(a,r)=>{const i=_(a);let t;return r===s.STANDARD?t=await o(()=>import("./index-Bp4RqK05.js").then(e=>e.I),[]):t=await o(()=>import("./index-Uc4Xhv31.js").then(e=>e.I),[]),t[i]};export{c as splitPathsBySizeLoader};
